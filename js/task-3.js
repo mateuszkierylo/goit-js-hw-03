cf5c83cbbc2693be5d8e51eb76e7dbcd307fcc25
mateuszkierylo <mateuszkierylo@gmail.com>
2024-09-27 10:01:31 +0200

    adding files

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,17 @@
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border)
+function filterArray(numbers, value) {
+    const result = [];
+    
+    for (const number of numbers) {
+        if (number > value) {
+            result.push(number)
+        }
+    }
+
+    return result;
 }
 
-
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
